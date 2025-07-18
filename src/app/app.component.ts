8ece9784c06b5e494077f0b39a35d6382c983dde
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2025-07-18 11:25:21 +0200

    Finish second exercise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,17 +4,48 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   standalone: true,
   template: `
-  <p></p>
+    <article class="offer">
+      <h1>
+        <span>Bonus Offer</span>
+        <span>&dollar;<!-- ITEM PRICE -->{{ item.price }}</span>
+      </h1>
+      <img src="/assets/noun-product-6277512.png" width="400" />
+      <p>
+        <!-- ITEM NAME-->
+        {{ item.name }}
+      </p>
+      <p><!-- ITEM DESCRIPTION -->{{ item.description }}</p>
+      <button>Order Now</button>
+    </article>
   `,
-  styles: [
-    `
-      ol {
-        color: red;
-        list-style-type: upper-roman;
-      }
-    `,
-  ],
+  styles: `
+    .offer {
+      font-family: Verdana;
+      border: solid 1px gray;
+      width: 400px;
+      padding: 20px;
+      border-radius: 3px;
+      color: white;
+      background: #7f6b41;
+    }
+    h1 {
+      display: flex;
+      justify-content: space-between;
+    }
+    button {
+      display: block;
+      width: 100%;
+      padding: 10px;
+      border: solid 1px white;
+      border-radius: 3px;
+    }
+  `,
 })
 export class AppComponent {
-  userName = "MarioUser"
+  item = {
+    name: 'Treasure Trove Trunk',
+    price: 30,
+    description:
+      'Unveil a treasure trove of surprises in this delightful mystery box.',
+  };
 }
