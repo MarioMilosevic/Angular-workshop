78bf16852ff322414f438b46f343d43c8403df69
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2025-07-19 18:46:38 +0200

    Finish 5th exercise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,92 @@
 import { Component } from '@angular/core';
-import { AccountInfo } from './account-info';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   template: `
-    <article class="card">
-      <h1 class="card-title">Frontend Masters Express</h1>
-      <p class="card-number">0000 0000 0000 0000</p>
-      <section class="membership-info">
-        <p>
-          {{ account.name }}
-        </p>
-        <p>Valid Thru: {{ account.validThru }}</p>
-        <p>CVV: {{ account.CVV }}</p>
-        <p>
-          @if(account.membershipStatus === 'gold') {
-          <span class="badge gold">Gold</span>
-          } @else if (account.membershipStatus === "platinum") {
-          <span class="badge platinum">Platinum</span>
-          } @else {
-          <span class="badge silver">Silver</span>
-          }
-        </p>
-      </section>
-    </article>
+    <section class="container">
+      <!-- This article element represents and entire listing -->
+      @for(car of carList;track car){
+      <article class="listing">
+        <div class="image-parent">
+          <img class="product-image" src="https://placehold.co/100x100" />
+        </div>
+        <section class="details">
+          <p class="title">
+            {{ car.make }} {{ car.model }}
+            <!-- car make and model-->
+          </p>
+          <hr />
+          <p class="detail">
+            <span>Year</span>
+            <span
+              >{{ car.year
+              }}<!-- year --></span
+            >
+          </p>
+          <div class="detail">
+            <span>Transmission</span>
+            <span
+              >{{ car.transmission
+              }}<!-- transmission --></span
+            >
+          </div>
+          <p class="detail">
+            <span>Mileage</span>
+            <span>
+              {{ car.miles }}
+              <!-- miles --></span
+            >
+          </p>
+          <p class="detail">
+            <span>Price</span>
+            <span>
+              {{ car.price }}
+              <!-- price --></span
+            >
+          </p>
+        </section>
+      </article>
+      } @empty {
+      <p>List is empty</p>
+      }
+    </section>
   `,
-  styleUrl: './app.component.css',
+  styleUrl: 'app.component.css',
 })
 export class AppComponent {
-  account: AccountInfo = {
-    name: 'Melisa Evan',
-    membershipStatus: 'silver',
-    validThru: '12/2022',
-    CVV: '123',
-  };
+  carList = [
+    {
+      make: 'Foyoda',
+      model: 'Famery',
+      miles: 54354,
+      price: 1000,
+      year: 2022,
+      transmission: 'Automatic',
+    },
+    {
+      make: 'Ronda',
+      model: 'Disaccord',
+      miles: 100000,
+      price: 230,
+      year: 1991,
+      transmission: 'Automatic',
+    },
+    {
+      make: 'Specific Motors',
+      model: 'Spoke',
+      miles: 100000,
+      price: 230,
+      year: 1991,
+      transmission: 'Automatic',
+    },
+    {
+      make: 'Fjord',
+      model: 'Pocus',
+      miles: 1,
+      price: 22330,
+      year: 2023,
+      transmission: 'Automatic',
+    },
+  ];
 }
